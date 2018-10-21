9bd9795f1cd809257ee0d2b928d9bf0539122718
Shawn Clake <shawn.clake@gmail.com>
2018-10-21 16:57:16 -0600

    Version updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name='apitaxdrivers',
     packages=find_packages(),  # this must be the same as the name above
-    version='0.0.8',
+    version='0.0.9',
     description='A set of example and micro drivers which can be used as the basis of more robust API drivers or combined together to form a robust application.',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,8 +25,8 @@ setup(
         'python-gitlab',
         'pygithub',
         'gitpython',
-        'apitaxcore==3.0.5',
-        'commandtax==0.0.6',
-        'scriptax==0.0.3'
+        'apitaxcore==3.0.6',
+        'commanndtax==0.0.8',
+        'scriptax==0.0.4',
     ],
 )
